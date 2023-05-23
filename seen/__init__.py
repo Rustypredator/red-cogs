317bfd0404288717df15c0d4c3b8562c25d0e433
Rustypredator <contact@rusty.info>
2023-05-23 22:31:31 +0200

    fixes load on 3.5.1

--- seen/__init__.py
+++ seen/__init__.py
@@ -1,10 +1,12 @@
 from .seen import Seen
 
 __red_end_user_data_statement__ = (
-    "Stores Discord IDs and timestamps for activity tracking."
+    "This cog does not persistently store end user data. "
+    "This cog does store discord IDs and last seen timestamp as needed for operation. "
 )
+
 
 async def setup(bot):
     cog = Seen(bot)
     await cog.initialize()
-    bot.add_cog(cog)
+    await bot.add_cog(cog)
